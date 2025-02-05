71b91f9559709f0c3d8ed73aa7d3864487d25310
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-02-05 01:42:04 +0100

    feat: add umd export

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
@@ -20,6 +20,13 @@ export default defineConfig({
                     format: "cjs",
                     entryFileNames: "index.cjs.js",
                     dir: "dist",
+                },
+                {
+                    format: "umd",
+                    entryFileNames: "index.umd.js",
+                    dir: "dist",
+                    name: "Breviarium", // Global variable for browsers
+                    sourcemap: true,
                 },
             ],
         },
