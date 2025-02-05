b74b05e73aa332dafa13ea5d5e6a89cce0c61620
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-02-05 01:26:56 +0100

    feat: add cjs and esm.js modules

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,23 @@ export default defineConfig({
             entry: "src/index.ts",
             name: "Breviarium",
             fileName: "breviarium",
+            formats: ["es", "cjs"], // Output ESM and CommonJS
+        },
+        rollupOptions: {
+            output: [
+                {
+                    format: "es",
+                    entryFileNames: "index.esm.js",
+                    dir: "dist",
+                    sourcemap: true,
+                },
+                {
+                    format: "cjs",
+                    entryFileNames: "index.cjs.js",
+                    dir: "dist",
+                    sourcemap: true,
+                },
+            ],
         },
     },
     plugins: [dts()],
