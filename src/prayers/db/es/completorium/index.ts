b1870e5e661cdcd34b038933e046608ed93279dd
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-03-25 03:48:33 +0100

    feat: add types for all prayers

---
+++ src/prayers/db/es/completorium/index.ts
@@ -0,0 +1,9 @@
+import sunday from "./1.json";
+import monday from "./2.json";
+import tuesday from "./3.json";
+import wednesday from "./4.json";
+import thursday from "./5.json";
+import friday from "./6.json";
+import saturday from "./7.json";
+
+export {sunday, monday, tuesday, wednesday, thursday, friday, saturday};
