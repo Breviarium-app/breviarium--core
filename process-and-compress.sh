ec800cac7e6c2e3a5f16d5657b31c07b7ab4d5bd
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-04-25 13:50:22 +0200

    feat: update responsorios remove annotation for easter and just display content

--- process-and-compress.sh
+++ process-and-compress.sh
@@ -8,20 +8,24 @@ cd ./src/prayers/db/ || exit
 find . -type f -exec sed -i '' 's/"primer_salmo_cita"/"s1_c"/g' {} +
 find . -type f -exec sed -i '' 's/"primer_salmo_antifona"/"s1_a"/g' {} +
 find . -type f -exec sed -i '' 's/"primer_salmo_texto"/"s1_t"/g' {} +
+echo "primer_salmo compressed 🗜️"
 
 # Replace keys related to segundo salmo
 find . -type f -exec sed -i '' 's/"segundo_salmo_cita"/"s2_c"/g' {} +
 find . -type f -exec sed -i '' 's/"segundo_salmo_antifona"/"s2_a"/g' {} +
 find . -type f -exec sed -i '' 's/"segundo_salmo_texto"/"s2_t"/g' {} +
+echo "segundo_salmo compressed 🗜️"
 
 # Replace keys related to tercer salmo
 find . -type f -exec sed -i '' 's/"tercer_salmo_cita"/"s3_c"/g' {} +
 find . -type f -exec sed -i '' 's/"tercer_salmo_antifona"/"s3_a"/g' {} +
 find . -type f -exec sed -i '' 's/"tercer_salmo_texto"/"s3_t"/g' {} +
+echo "tercer_salmo compressed 🗜️"
 
 # Replace keys related to lectura biblica
 find . -type f -exec sed -i '' 's/"lectura_biblica_cita"/"lb_c"/g' {} +
 find . -type f -exec sed -i '' 's/"lectura_biblica"/"lb_t"/g' {} +
+echo "lectura_biblica compressed 🗜️"
 
 # Replace other keys
 find . -type f -exec sed -i '' 's/"cantico_evangelico_antifona"/"ce_a"/g' {} +
@@ -30,7 +34,7 @@ find . -type f -exec sed -i '' 's/"preces_respuesta"/"preces_r"/g' {} +
 find . -type f -exec sed -i '' 's/"preces_contenido"/"preces_c"/g' {} +
 find . -type f -exec sed -i '' 's/"invitacion_padrenuestro"/"invi_pn"/g' {} +
 find . -type f -exec sed -i '' 's/"oracion_final"/"o_final"/g' {} +
-
+echo "preces, cantico_evangelico, preces, oracion_final compressed 🗜️"
 # Officium
 
 # Replace keys related to lectura biblica
@@ -39,3 +43,4 @@ find . -type f -exec sed -i '' 's/"lectura_biblica_texto"/"lb_texto"/g' {} +
 find . -type f -exec sed -i '' 's/"lectura_patristica_titulo"/"lp_tit"/g' {} +
 find . -type f -exec sed -i '' 's/"lectura_patristica_cita"/"lp_c"/g' {} +
 find . -type f -exec sed -i '' 's/"lectura_patristica_texto"/"lp_texto"/g' {} +
+echo "lectura_biblica, lectura_patristica compressed 🗜️"
