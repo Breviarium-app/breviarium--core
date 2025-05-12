242abeb6110facf76895c511e411d6efe0f890c7
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-05-12 16:37:15 +0200

    feat: add test getLiturgyInformation

--- src/__tests__/tests/breviarium.test.ts
+++ src/__tests__/tests/breviarium.test.ts
@@ -139,4 +139,24 @@ describe("Breviarium module", () => {
         assert(result !== undefined);
     });
 
+    it("getLiturgyInformation", async () => {
+        const breviarium = new Breviarium();
+        const result = await breviarium.getLiturgyInformation(
+            new Date(2025, 0, 1))
+        console.log(result)
+
+        const expected = {
+            psaltery_week: 'WEEK_1',
+            cycle: 'YEAR_C',
+            color: 'WHITE',
+            color_hex: '#bdbdbd66'
+        }
+
+        assert(result !== undefined);
+        expect(result).to.deep.equal(expected);
+        expect(expected.psaltery_week).to.be.equal(result.psaltery_week);
+        expect(expected.cycle).to.be.equal(result.cycle);
+        expect(expected.color).to.be.equal(result.color);
+    });
+
 });
