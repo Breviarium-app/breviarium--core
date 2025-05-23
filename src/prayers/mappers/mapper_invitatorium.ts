82eb7c3d521df9541098eb99f648a3bbc3e8e66c
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-05-23 02:36:29 +0200

    feat: mapper invitatorium fix

--- src/prayers/mappers/mapper_invitatorium.ts
+++ src/prayers/mappers/mapper_invitatorium.ts
@@ -2,8 +2,5 @@ import {InvitatoriumSchemaOutput} from "@/prayer-manager-interface.ts";
 import {findText} from "@/prayers/mappers/findText";
 
 export function mapper_invitatorium(item: any): InvitatoriumSchemaOutput | undefined {
-
-    item.val = findText('invitatorios', item.val);
-
-    return item;
+    return {'id': item.id, 'val': findText('invitatorios', item.val)};
 }
